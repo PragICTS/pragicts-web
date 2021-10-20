a8c52094c26606f6a15345a5b64798c2d006e529
Althaf Nawshad <althaf@pragicts.com>
2021-10-20 07:47:08 +0530

    Updates

---
+++ component---src-pages-about-us-js-e1a10dc1e63e4ceee4fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpragicts=self.webpackChunkpragicts||[]).push([[539],{9315:function(e,t,r){r.r(t);var a=r(7294),u=r(7726);t.default=function(){return a.createElement(u.Z,{pageTitle:"About PragICTS"},a.createElement("div",{style:{backgroundColor:"white"}},"About Us"))}}}]);
+//# sourceMappingURL=component---src-pages-about-us-js-e1a10dc1e63e4ceee4fc.js.map
